d802d8b5634a52466863cf2714c29875045d2ecb
ayechan <ayechansant@gmail.com>
2024-03-12 00:49:58 -0700

    fixed tailwind not working

--- front-end-practice/src/App.tsx
+++ front-end-practice/src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import styles from "./styles";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div
+        className={`${styles.paddingX} ${styles.flexCenter}`}
+      >
+        <div className={`${styles.boxWidth}`}>NavBar</div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+      <div
+        className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}
+      >
+        <div className={`${styles.boxWidth}`}>
+          Stats Business Billing CardDeal Testimonials
+          Clients Cta
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ front-end-practice/src/components/Bussiness.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bussiness() {
+  return <div>Bussiness</div>;
+}
+
+export default Bussiness;
